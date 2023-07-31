0fad04260a16a0b2f52d77e2d67d915d0e38766b
ricca <giochi.ric05@gmail.com>
2023-07-31 09:37:21 +0200

    primo_caricamento

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.TesseractException;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -24,10 +25,10 @@ public class Main {
 
 
 
-       Tesseract tesseract = new Tesseract();
-        try {
+      // Tesseract tesseract = new Tesseract();
+      //  try {
             // Imposto il percorso dei dati linguistici (es. per l'italiano)
-            tesseract.setDatapath("C:\\Users\\ricca\\Downloads\\Tess4J-3.4.8-src\\Tess4J\\tessdata");
+            //tesseract.setDatapath("C:\\Users\\ricca\\Downloads\\Tess4J-3.4.8-src\\Tess4J\\tessdata");
 
             // Carica l'immagine contenente il testo da riconoscere
            //File imageFile = new File("C:\\Users\\ricca\\OneDrive\\Desktop\\Tesi\\immagine");
@@ -35,11 +36,21 @@ public class Main {
             // Esegui l'OCR sull'immagine
            // String extractedText = tesseract.doOCR(imageFile);
            // System.out.println("Testo estratto: " + extractedText);
-            String text = tesseract.doOCR(new File("C:\\Users\\ricca\\OneDrive\\Desktop\\Tesi\\immagine.png"));
+            /*String text = tesseract.doOCR(new File("C:\\Users\\ricca\\OneDrive\\Desktop\\Tesi\\immagine.png"));
             System.out.println("Output: " + text);
         } catch (TesseractException e) {
             System.err.println("Errore durante l'OCR: " + e.getMessage());
-        }
+        }*/
+
+            File imageFile = new File("C:\\Users\\ricca\\OneDrive\\Desktop\\Tesi\\immagine.png");
+            ITesseract instance = new Tesseract();
+            instance.setDatapath("C:\\Users\\ricca\\IdeaProjects\\Tesi\\src\\main\\tessdata");
+            try {
+                String result = instance.doOCR(imageFile);
+                System.out.println(result);
+            } catch (TesseractException e) {
+                System.err.println(e.getMessage());
+            }
     }
 }
 
